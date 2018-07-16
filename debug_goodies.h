6b27141faf67ca1faa4cab90e5b5acbcc8b1e66d
Mateusz Dudzinski <mateuszpd7@gmail.com>
2018-07-16 20:35:44 +0200

    Finished the cleanup.

--- debug_goodies.h
+++ debug_goodies.h
@@ -12,22 +12,22 @@
 // * BENCHMARK_BEFORE_MAIN(NAME, REPEAT, EXPR)
 
 // Define 'LOGGING' to enable:
-// * LOG_TRACE
-// * LOG_DEBUG
+// * LOG_TRACE (Only if 'DEBUG' is defined)
+// * LOG_DEBUG (Only if 'DEBUG' is defined)
 // * LOG_INFO
 // * LOG_WARN
 // * LOG_ERROR
 // * LOG_FATAL
 
 // Other definable stuff:
-// * LOG_GODIES_USE_DATE (To add date & time to your logs)
-// * DEBUG_GOODIES_FPRINTF (If you don't want to use fprintf)
-// * DEBUG_GODIES_BENCHMARK_FILE (To redirect benchmarks results)
-// * DEBUG_GODIES_LOG_FILE (To redirect logs to the file, not stderr)
-// * DEBUG_PANIC_BEHAVIOUR(MSG, ...) (Panic logic if defined DEBUG)
-// * RELEASE_PANIC_BEHAVIOUR(MSG, ...) (Panic logic if not defined DEBUG)
-// * TIME_SCOPE_MSG(NAME, TIME) (Logic when time scope ends)
-// * BENCHMARK_MSG(NAME, REPEAT, BEST, WORST, AVG) (Logic after benchmark)
+// * DG_USE_DATE (To add date & time to your logs)
+// * DG_FPRINTF (If you don't want to use fprintf)
+// * DG_BENCHMARK_FILE (To redirect benchmarks results)
+// * DG_LOG_FILE (To redirect logs to the file, not stderr)
+// * DG_DEBUG_PANIC_BEHAVIOUR(MSG, ...) (Panic logic if defined DEBUG)
+// * DG_RELEASE_PANIC_BEHAVIOUR(MSG, ...) (Panic logic if not defined DEBUG)
+// * DG_TIME_SCOPE_MSG(NAME, TIME) (Logic when time scope ends)
+// * DG_BENCHMARK_MSG(NAME, REPEAT, BEST, WORST, AVG) (Logic after benchmark)
 
 // TODO: Add ability not to use chrono.
 
@@ -36,66 +36,58 @@
 
 // ------------------- AUXILIARY -------------------
 
-#define ___CONCATENATE_IMPL(NAME1, NAME2) NAME1##NAME2
-#define ___CONCATENATE(NAME1, NAME2) ___CONCATENATE_IMPL(NAME1, NAME2)
+#define DG_INT_CONCAT_IMPL(NAME1, NAME2) NAME1##NAME2
+#define DG_INT_CONCAT(NAME1, NAME2) DG_INT_CONCAT_IMPL(NAME1, NAME2)
 
 #ifdef __COUNTER__
-  #define ANONYMUS_NAME(NAME) ___CONCATENATE(NAME, __COUNTER__)
+  #define DG_INT_ANON_NAME(NAME) DG_INT_CONCAT(NAME, __COUNTER__)
 #else
-  #define ANONYMUS_NAME(NAME) ___CONCATENATE(NAME, __LINE__)
+  #define DG_INT_ANON_NAME(NAME) DG_INT_CONCAT(NAME, __LINE__)
 #endif
-
-#define ___RUN_BEFORE_MAIN_AUX(NAME, EXPR)                               \
-    struct NAME                                                          \
-    {                                                                    \
-        NAME() { EXPR; }                                                 \
-    }; NAME ANONYMUS_NAME(___ANON_VARIABLE_NAME___)
-
-#define RUN_BEFORE_MAIN(EXPR)                                            \
-    ___RUN_BEFORE_MAIN_AUX(ANONYMUS_NAME(___ANON_VARIABLE_NAME___), EXPR)
 
 // ------------------- PLATFORM AND DEFAULTS -------------------
 
 // Cross platform debug break.
+// TODO: Tests it on clang under WIN32 machine.
 #if defined(__clang__)
   #include <signal.h>
-  #define ___DEBUG_BREAK() raise(SIGTRAP)
+  #define DG_INT_BREAK() raise(SIGTRAP)
 #elif defined(__GNUC__) || defined(__GNUG__)
   #include <signal.h>
-  #define ___DEBUG_BREAK() raise(SIGTRAP)
+  #define DG_INT_BREAK() raise(SIGTRAP)
 #elif defined(_MSC_VER)
-  #define ___DEBUG_BREAK() __debugbreak()
+  #define DG_INT_BREAK() __debugbreak()
 #endif
 
-// Define 'DEBUG_GOODIES_FPRINTF' if you dont want to use fprintf.
-#ifndef DEBUG_GOODIES_FPRINTF
+// Define 'DG_FPRINTF' if you dont want to use fprintf.
+#ifndef DG_FPRINTF
   #include <cstdio>
-  #define DEBUG_GOODIES_FPRINTF fprintf
+  #define DG_FPRINTF fprintf
 #endif
 
-// Define 'DEBUG_GODIES_LOG_FILE' for benchmarks output. Default is stdout.
-#ifndef DEBUG_GODIES_BENCHMARK_FILE
+// Define 'DG_BENCHMARK_FILE' for benchmarks output. Default is stdout.
+#ifndef DG_BENCHMARK_FILE
   #include <cstdio>
-  #define DEBUG_GODIES_BENCHMARK_FILE stdout
+  #define DG_BENCHMARK_FILE stdout
 #endif
 
-// Define 'DEBUG_GODIES_LOG_FILE' to set log output. Default is stderr.
-#ifndef DEBUG_GODIES_LOG_FILE
+// Define 'DG_LOG_FILE' to set log output. Default is stderr.
+#ifndef DG_LOG_FILE
   #include <cstdio>
-  #define DEBUG_GODIES_LOG_FILE stderr
+  #define DG_LOG_FILE stderr
 #endif
 
-// Define 'LOG_GODIES_USE_DATE' for time info in logs, requires chorno
-#ifdef LOG_GODIES_USE_DATE
+// Define 'DG_USE_DATE' for time info in logs, requires chorno
+#ifdef DG_USE_DATE
   #include <chrono>
-  #define ____PRINT_DATETIME_TO_FILE(_FILE)                              \
+  #define DG_INT_DATETIME_TO_FILE(_FILE)                                 \
       do {                                                               \
           auto in_time_t =                                               \
               std::chrono::system_clock::to_time_t(                      \
                   std::chrono::system_clock::now());                     \
           auto tm_info = localtime(&in_time_t);                          \
                                                                          \
-          DEBUG_GOODIES_FPRINTF(                                         \
+          DG_FPRINTF(                                                    \
               _FILE,                                                     \
               "%02d-%02d-%d %02d:%02d:%02d: ",                           \
               tm_info->tm_year + 1900, tm_info->tm_mon + 1,              \
@@ -103,57 +95,57 @@
               tm_info->tm_min, tm_info->tm_sec);                         \
       } while(0)
 #else
-  #define ____PRINT_DATETIME_TO_FILE(IGNORE) ((void)0)
+  #define DG_INT_DATETIME_TO_FILE(IGNORE) ((void)0)
 #endif
 
-#define ___LOG(MSG, LOG_TYPE, ...)                                       \
+#define DG_INT_LOG(MSG, LOG_TYPE, ...)                                   \
     do {                                                                 \
-        DEBUG_GOODIES_FPRINTF(DEBUG_GODIES_LOG_FILE,                     \
+        DG_FPRINTF(DG_LOG_FILE,                                          \
                               "%s:%d: %s: ",                             \
                               __FILE__, __LINE__, #LOG_TYPE);            \
-        ____PRINT_DATETIME_TO_FILE(DEBUG_GODIES_LOG_FILE);               \
-        DEBUG_GOODIES_FPRINTF(DEBUG_GODIES_LOG_FILE, MSG,                \
+        DG_INT_DATETIME_TO_FILE(DG_LOG_FILE);                            \
+        DG_FPRINTF(DG_LOG_FILE, MSG,                                     \
                               ##__VA_ARGS__);                            \
-        DEBUG_GOODIES_FPRINTF(DEBUG_GODIES_LOG_FILE, "\n");              \
+        DG_FPRINTF(DG_LOG_FILE, "\n");                                   \
 } while(0)
 
-// Default panic behaviour. If either 'DEBUG_PANIC_BEHAVIOUR' or
-// 'RELEASE_PANIC_BEHAVIOUR' are not defined this is the default.
-#define DEFAULT_PANIC_BEHAVIOUR(MSG, ...)                                \
+// Default panic behaviour. If either 'DG_DEBUG_PANIC_BEHAVIOUR' or
+// 'DG_RELEASE_PANIC_BEHAVIOUR' are not defined this is the default.
+#define DG_INT_DEFAULT_PANIC_BEHAVIOUR(MSG, ...)                         \
     do {                                                                 \
-        ___LOG(MSG, PANIC, ##__VA_ARGS__);                               \
+        DG_INT_LOG(MSG, PANIC, ##__VA_ARGS__);                           \
     } while(0)
 
 // Panic macro defaults.
 // These can be redefined to do whatever user wants.
-#ifndef DEBUG_PANIC_BEHAVIOUR
-  #define DEBUG_PANIC_BEHAVIOUR(MSG, ...)                                \
-      DEFAULT_PANIC_BEHAVIOUR(MSG, ##__VA_ARGS__)
+#ifndef DG_DEBUG_PANIC_BEHAVIOUR
+  #define DG_DEBUG_PANIC_BEHAVIOUR(MSG, ...)                             \
+      DG_INT_DEFAULT_PANIC_BEHAVIOUR(MSG, ##__VA_ARGS__)
 #endif
 
-#ifndef RELEASE_PANIC_BEHAVIOUR
-  #define RELEASE_PANIC_BEHAVIOUR(MSG, ...)                              \
-      DEFAULT_PANIC_BEHAVIOUR(MSG, ##__VA_ARGS__)
+#ifndef DG_RELEASE_PANIC_BEHAVIOUR
+  #define DG_RELEASE_PANIC_BEHAVIOUR(MSG, ...)                           \
+      DG_INT_DEFAULT_PANIC_BEHAVIOUR(MSG, ##__VA_ARGS__)
 #endif
 
 // Logic that takes place when single 'POP_TIMER' is done.
-#ifndef TIME_SCOPE_MSG
-  #define TIME_SCOPE_MSG(NAME, TIME)                                     \
-      ___LOG("%s - %.3fs", "TIMED: ", NAME, (float)TIME / 1000)
+#ifndef DG_TIME_SCOPE_MSG
+  #define DG_TIME_SCOPE_MSG(NAME, TIME)                                  \
+      DG_INT_LOG("%s - %.3fs", "TIMED: ", NAME, (float)TIME / 1000)
 #endif
 
 // Logic that takes place when after the benchmark is done.
-#ifndef BENCHMARK_MSG
-  #define BENCHMARK_MSG(NAME, REPEAT, BEST, WORST, AVG)                  \
-    DEBUG_GOODIES_FPRINTF(DEBUG_GODIES_BENCHMARK_FILE,                   \
-        "%s:%d: BENCH: %s(x%d) - AVG: %.3fs, BEST: %.3fs, WORST: %.3fs\n", \
+#ifndef DG_BENCHMARK_MSG
+  #define DG_BENCHMARK_MSG(NAME, REPEAT, BEST, WORST, AVG)               \
+    DG_FPRINTF(DG_BENCHMARK_FILE,                                        \
+      "%s:%d: BENCH: %s(x%d) - AVG: %.3fs, BEST: %.3fs, WORST: %.3fs\n", \
         __FILE__, __LINE__, NAME, REPEAT,                                \
         (float)AVG / 1000, (float)BEST / 1000, (float)WORST / 1000)
 #endif
 
 // ------------------- DEBUG BREAK -------------------
 
-#define BREAK() ___DEBUG_BREAK()
+#define BREAK() DG_INT_BREAK()
 
 // ------------------- ASSERTIONS -------------------
 
@@ -163,13 +155,13 @@
 #ifdef DEBUG
   #define PANIC(MSG, ...)                                                \
       do {                                                               \
-          DEBUG_PANIC_BEHAVIOUR(MSG, ##__VA_ARGS__);                     \
+          DG_DEBUG_PANIC_BEHAVIOUR(MSG, ##__VA_ARGS__);                  \
           BREAK();                                                       \
       } while(0)
 #else
   #define PANIC(MSG, ...)                                                \
       do {                                                               \
-          RELEASE_PANIC_BEHAVIOUR(MSG, ##__VA_ARGS__);                   \
+          DG_RELEASE_PANIC_BEHAVIOUR(MSG, ##__VA_ARGS__);                \
       } while(0)
 #endif
 
@@ -181,7 +173,7 @@
       do {                                                               \
           if (!(expr))                                                   \
           {                                                              \
-              ___LOG("%s", ASSERT_FAIL, #EXPR);                          \
+              DG_INT_LOG("%s", ASSERT_FAIL, #EXPR);                      \
               BREAK();                                                   \
           }                                                              \
       } while(0)
@@ -266,7 +258,7 @@
                   std::chrono::system_clock::now() -                     \
                   debug::detail::counters_stack[debug::detail::index]    \
                       .timer);                                           \
-          TIME_SCOPE_MSG(                                                \
+          DG_TIME_SCOPE_MSG(                                             \
               debug::detail::counters_stack[debug::detail::index].name,  \
               diff.count());                                             \
       } while(0)
@@ -296,14 +288,14 @@
               sum += _bench_res_;                                        \
           }                                                              \
           float avg = sum / REPEAT;                                      \
-          BENCHMARK_MSG(NAME, REPEAT, best, worse, avg);                 \
+          DG_BENCHMARK_MSG(NAME, REPEAT, best, worse, avg);              \
       } while (0);
 #else
   #define RUN_BENCHMARK(IGNORE1, IGNORE2, IGNORE3) ((void)0)
 #endif
 
 #ifdef BENCHMARK
- #define ___BENCHMARK_BEFORE_MAIN_AUX(NAME, BNAME, REPEAT, EXPR)         \
+#define DG_INT_BENCHMARK_BEFORE_MAIN_AUX(NAME, BNAME, REPEAT, EXPR)      \
      struct NAME                                                         \
      {                                                                   \
          NAME()                                                          \
@@ -326,27 +318,24 @@
                  sum += _bench_res_;                                     \
              }                                                           \
              float avg = sum / REPEAT;                                   \
-             BENCHMARK_MSG(BNAME, REPEAT, best, worst, avg);             \
+             DG_BENCHMARK_MSG(BNAME, REPEAT, best, worst, avg);          \
          }                                                               \
-     }; NAME ANONYMUS_NAME(___ANON_VARIABLE_NAME___)
+     }; NAME DG_INT_ANON_NAME(___ANON_VARIABLE_NAME___)
 #endif
 
 #ifdef BENCHMARK
   #define BENCHMARK_BEFORE_MAIN(NAME, REPEAT, EXPR)                      \
-    ___BENCHMARK_BEFORE_MAIN_AUX(                                        \
-        ANONYMUS_NAME(___ANON_VARIABLE_NAME___), NAME, REPEAT, EXPR)
+      DG_INT_BENCHMARK_BEFORE_MAIN_AUX(                                  \
+        DG_INT_ANON_NAME(___ANON_VARIABLE_NAME___), NAME, REPEAT, EXPR)
 #else
   #define BENCHMARK_BEFORE_MAIN(NAME, REPEAT, EXPR) ;
 #endif
 
 // ------------------- LOGGING -------------------
 
-// TODO: Refactor to dont use fprintf.
-// TODO: Add info about functionality.
-
 #if defined(DEBUG) && defined(LOGGING)
-  #define LOG_TRACE(MSG, ...) ___LOG(MSG, TRACE, ##__VA_ARGS__)
-  #define LOG_DEBUG(MSG, ...) ___LOG(MSG, DEBUG, ##__VA_ARGS__)
+  #define LOG_TRACE(MSG, ...) DG_INT_LOG(MSG, TRACE, ##__VA_ARGS__)
+  #define LOG_DEBUG(MSG, ...) DG_INT_LOG(MSG, DEBUG, ##__VA_ARGS__)
 #elif defined(LOGGING)
   #define LOG_TRACE(IGNORE, ...) ((void)0)
   #define LOG_DEBUG(IGNORE, ...) ((void)0)
@@ -354,10 +343,43 @@
 #endif
 
 #ifdef LOGGING
-  #define LOG_INFO(MSG, ...) ___LOG(MSG, INFO, ##__VA_ARGS__)
-  #define LOG_WARN(MSG, ...) ___LOG(MSG, WARN, ##__VA_ARGS__)
-  #define LOG_ERROR(MSG, ...) ___LOG(MSG, ERROR, ##__VA_ARGS__)
-  #define LOG_FATAL(MSG, ...) ___LOG(MSG, FATAL, ##__VA_ARGS__)
+  #define LOG_INFO(MSG, ...) DG_INT_LOG(MSG, INFO, ##__VA_ARGS__)
+  #define LOG_WARN(MSG, ...) DG_INT_LOG(MSG, WARN, ##__VA_ARGS__)
+  #define LOG_ERROR(MSG, ...) DG_INT_LOG(MSG, ERROR, ##__VA_ARGS__)
+  #define LOG_FATAL(MSG, ...) DG_INT_LOG(MSG, FATAL, ##__VA_ARGS__)
+#endif
+
+// Undef auxiliary macros.
+#if DG_INT_CONCAT_IMPL
+  #undef DG_INT_CONCAT_IMPL
+#endif
+
+#if DG_INT_CONCAT
+  #undef DG_INT_CONCAT
+#endif
+
+#if DG_INT_ANON_NAME
+  #undef DG_INT_ANON_NAME
+#endif
+
+#if DG_INT_BREAK
+  #undef DG_INT_BREAK
+#endif
+
+#if DG_INT_DATETIME_TO_FILE
+  #undef DG_INT_DATETIME_TO_FILE
+#endif
+
+#if DG_INT_LOG
+  #undef DG_INT_LOG
+#endif
+
+#if DG_INT_DEFAULT_PANIC_BEHAVIOUR
+  #undef DG_INT_DEFAULT_PANIC_BEHAVIOUR
+#endif
+
+#if DG_INT_BENCHMARK_BEFORE_MAIN_AUX
+  #undef DG_INT_BENCHMARK_BEFORE_MAIN_AUX
 #endif
 
 #endif // __DEBUG_GOODIES_H__
